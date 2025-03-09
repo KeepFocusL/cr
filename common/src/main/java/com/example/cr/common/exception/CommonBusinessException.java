cf099fd19a09475de7a01d154c3c24647033d08a
KeepFocusL <1281438594@qq.com>
2025-03-09 15:30:11 +0800

    feat(user): 增加用户登录接口

---
+++ common/src/main/java/com/example/cr/common/exception/CommonBusinessException.java
@@ -0,0 +1,7 @@
+package com.example.cr.common.exception;
+
+public class CommonBusinessException extends CustomException {
+    public CommonBusinessException(String message) {
+        super(message);
+    }
+}
---
+++ common/src/main/java/com/example/cr/common/exception/UserNotExistsException.java
@@ -0,0 +1,7 @@
+package com.example.cr.common.exception;
+
+public class UserNotExistsException extends CustomException {
+    public UserNotExistsException(String message) {
+        super(message);
+    }
+}
