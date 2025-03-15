cd9614b6ed2af9628d91ab5b9ce128806191d626
KeepFocusL <1281438594@qq.com>
2025-03-15 16:30:09 +0800

    feat(web): 真实请求后端的测试接口并把返回值显示在 Dashboard 页面中 ping/pong 同时检查请求头是否带了 token

--- web/src/views/Dashboard.vue
+++ web/src/views/Dashboard.vue
@@ -1,5 +1,15 @@
 <script setup>
+import {onMounted, ref} from "vue";
+import {ping} from '@/api/auth.js'
 
+const content = ref('')
+
+onMounted(() => {
+  ping()
+    .then((res) => {
+      content.value = res.data
+    })
+})
 </script>
 
 <template>
@@ -7,6 +17,7 @@
     <h1>
       这是一个 Dashboard 页
     </h1>
+    <p>ping : {{ content }}</p>
   </div>
 </template>
 
