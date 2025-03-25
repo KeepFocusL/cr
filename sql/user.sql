0a87a859116f1816233fa7c128d71a53c906092c
KeepFocusL <1281438594@qq.com>
2025-03-25 15:31:28 +0800

    feat(generator): 添加测试使用的 sb 表

--- sql/user.sql
+++ sql/user.sql
@@ -11,3 +11,18 @@ create table `user`
     `updated_at` datetime(3) null comment '更新时间', -- 例如，2025-01-01 12:34:56.789
     unique key `mobile_unique` (`mobile`)
 ) comment ='用户';
+
+
+-- sb
+drop table if exists `sb`;
+create table `sb`
+(
+    `id`         bigint primary key,
+    `name`       varchar(20) null COMMENT '傻逼用户名',
+    `email`      varchar(50) null COMMENT '傻逼邮箱',
+    `password`   varchar(50) null COMMENT '傻逼密码',
+    `mobile`     varchar(11) not null comment '傻逼手机号',
+    `created_at` datetime(3) null comment '傻逼创建时间', -- 精度为 3 位小数秒，表示可以存储到毫秒级别。
+    `updated_at` datetime(3) null comment '傻逼更新时间', -- 例如，2025-01-01 12:34:56.789
+    unique key `mobile_unique` (`mobile`)
+) comment ='傻逼';
