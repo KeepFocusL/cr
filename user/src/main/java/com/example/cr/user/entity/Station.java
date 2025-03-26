6d51716704d31a0317a600cd5f4fa226063be511
KeepFocusL <1281438594@qq.com>
2025-03-26 19:02:28 +0800

    feat(user): 自动生成【车站】持久层代码

---
+++ user/src/main/java/com/example/cr/user/entity/Station.java
@@ -0,0 +1,92 @@
+package com.example.cr.user.entity;
+
+import java.util.Date;
+
+public class Station {
+    private Long id;
+
+    private String name;
+
+    private String namePinyin;
+
+    private String namePy;
+
+    private String description;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNamePinyin() {
+        return namePinyin;
+    }
+
+    public void setNamePinyin(String namePinyin) {
+        this.namePinyin = namePinyin;
+    }
+
+    public String getNamePy() {
+        return namePy;
+    }
+
+    public void setNamePy(String namePy) {
+        this.namePy = namePy;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", namePinyin=").append(namePinyin);
+        sb.append(", namePy=").append(namePy);
+        sb.append(", description=").append(description);
+        sb.append(", createdAt=").append(createdAt);
+        sb.append(", updatedAt=").append(updatedAt);
+        sb.append("]");
+        return sb.toString();
+    }
+}
