ff7d59dbb445c2e0528d22672c4eb59c9b913ce7
KeepFocusL <1281438594@qq.com>
2025-03-27 15:27:06 +0800

    feat(business): 增加 mybatis 相关的注解和配置
    - 增加 @MapperScan 注解
    - 注解:告诉他以后自动生成的 mapper.java 在哪里
    - 配置:告诉他以后自动生成的 mapper.xml 在哪里

--- business/src/main/java/com/example/cr/business/BusinessApplication.java
+++ business/src/main/java/com/example/cr/business/BusinessApplication.java
@@ -1,11 +1,13 @@
 package com.example.cr.business;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.example.cr")
+@MapperScan("com.example.cr.business.mapper")
 public class BusinessApplication {
     public static void main(String[] args) {
         SpringApplication.run(BusinessApplication.class, args);
