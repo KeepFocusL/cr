e9aa614a588882d58de4481ff32cb81bd2ad9f3c
KeepFocusL <1281438594@qq.com>
2025-03-27 14:32:46 +0800

    feat(business): 增加 @ComponentScan("com.example.cr") 注解
    由于依赖了 common 模块，增加 @ComponentScan("com.example.cr") 注解，覆盖 Spring Boot 默认的扫描规则
    
    common 中有些 Bean 需要被扫描进来

--- business/src/main/java/com/example/cr/business/BusinessApplication.java
+++ business/src/main/java/com/example/cr/business/BusinessApplication.java
@@ -2,8 +2,10 @@ package com.example.cr.business;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.cr")
 public class BusinessApplication {
     public static void main(String[] args) {
         SpringApplication.run(BusinessApplication.class, args);
