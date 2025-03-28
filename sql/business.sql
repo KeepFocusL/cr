9411a553550839b96d24b509fccc55de6ab26ffa
KeepFocusL <1281438594@qq.com>
2025-03-28 10:30:38 +0800

    fix(business): 想让哪个字段支持搜索就在哪个字段添加 |searchable

--- sql/business.sql
+++ sql/business.sql
@@ -2,9 +2,9 @@ drop table if exists `station`;
 create table `station`
 (
     `id`          bigint      not null comment 'id',
-    `name`        varchar(20) not null comment '站名',
-    `name_pinyin` varchar(50) not null comment '站名拼音',
-    `name_py`     varchar(50) not null comment '站名拼音首字母',
+    `name`        varchar(20) not null comment '站名|searchable',
+    `name_pinyin` varchar(50) not null comment '站名拼音|searchable',
+    `name_py`     varchar(50) not null comment '站名拼音首字母|searchable',
     `created_at` datetime(3) null comment '创建时间', -- 精度为 3 位小数秒，表示可以存储到毫秒级别。
     `updated_at` datetime(3) null comment '更新时间', -- 例如，2025-01-01 12:34:56.789
     primary key (`id`),
