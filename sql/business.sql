bdd5400c9df3483c1df2b86507fd24d099998ad9
KeepFocusL <1281438594@qq.com>
2025-03-30 10:26:45 +0800

    feat(business): 手动添加【火车车站】建表语句 + 建表

--- sql/business.sql
+++ sql/business.sql
@@ -29,3 +29,22 @@ create table `train`
     primary key (`id`),
     unique key `code_unique` (`code`)
 ) comment='车次';
+
+drop table if exists `train_station`;
+create table `train_station`
+(
+    `id`          bigint        not null comment 'id',
+    `train_code`  varchar(20)   not null comment '车次编号',
+    `index`       int           not null comment '站序',
+    `name`        varchar(20)   not null comment '站名',
+    `name_pinyin` varchar(50)   not null comment '站名拼音',
+    `in_time`     time comment '进站时间',
+    `out_time`    time comment '出站时间',
+    `stop_time`   time comment '停站时长',
+    `km`          decimal(8, 2) not null comment '里程（公里）|从上一站到本站的距离',
+    `created_at`  datetime(3) null comment '创建时间',
+    `updated_at`  datetime(3) null comment '更新时间',
+    primary key (`id`),
+    unique key `train_code_index_unique` (`train_code`, `index`),
+    unique key `train_code_name_unique` (`train_code`, `name`)
+) comment ='火车车站';
