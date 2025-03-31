1b9b6e3b76bf7e278802d1d725790c18b24833af
KeepFocusL <1281438594@qq.com>
2025-03-31 19:40:56 +0800

    feat(scheduler): 参考 business 模块增加多环境,数据库等相关配置

--- scheduler/src/main/java/com/example/cr/scheduler/SchedulerApplication.java
+++ scheduler/src/main/java/com/example/cr/scheduler/SchedulerApplication.java
@@ -1,11 +1,13 @@
 package com.example.cr.scheduler;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
     @SpringBootApplication
     @ComponentScan("com.example.cr")
+    @MapperScan("com.example.cr.scheduler.mapper")
     public class SchedulerApplication {
         public static void main(String[] args) {
             SpringApplication.run(SchedulerApplication.class, args);
