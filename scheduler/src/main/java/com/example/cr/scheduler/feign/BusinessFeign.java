0b3ea90b322e4984a0871ea9b7f9ffb9a242e301
KeepFocusL <1281438594@qq.com>
2025-04-12 13:49:48 +0800

    feat(scheduler): DailyTrainJob 跨服务调用 business 模块新增加的 gen-daily/{date} 接口

--- scheduler/src/main/java/com/example/cr/scheduler/feign/BusinessFeign.java
+++ scheduler/src/main/java/com/example/cr/scheduler/feign/BusinessFeign.java
@@ -1,10 +1,18 @@
 package com.example.cr.scheduler.feign;
 
+import com.example.cr.common.response.R;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Date;
 
 @FeignClient(name = "business", url = "http://localhost:8082")
 public interface BusinessFeign {
     @GetMapping("test")
     String test();
+
+    @GetMapping("admin/daily-train/gen-daily/{date}")
+    R<Object> genDaily(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Date date);
 }
--- scheduler/src/main/java/com/example/cr/scheduler/job/DailyTrainJob.java
+++ scheduler/src/main/java/com/example/cr/scheduler/job/DailyTrainJob.java
@@ -1,5 +1,8 @@
 package com.example.cr.scheduler.job;
 
+import cn.hutool.Hutool;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
 import com.example.cr.scheduler.feign.BusinessFeign;
 import jakarta.annotation.Resource;
 import org.quartz.DisallowConcurrentExecution;
@@ -7,6 +10,8 @@ import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Date;
 
 @DisallowConcurrentExecution
 public class DailyTrainJob implements Job {
@@ -18,8 +23,11 @@ public class DailyTrainJob implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) {
         log.info("生成每日数据 - 开始");
-        businessFeign.test();
+//        businessFeign.test();
         // 思考: 真正生成每日车次的步骤，流程
+        DateTime date = DateUtil.offsetDay(new Date(), 1);
+        System.out.println(date);
+        businessFeign.genDaily(date);
         log.info("生成每日数据 - 结束");
     }
 }
