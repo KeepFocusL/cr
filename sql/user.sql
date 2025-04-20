36ad48ca05651c0d5a118919ad8f44b5b34a7882
KeepFocusL <1281438594@qq.com>
2025-04-20 10:48:42 +0800

    fix(user): 修改 passenger 建表语句 【并执行】

--- sql/user.sql
+++ sql/user.sql
@@ -46,13 +46,13 @@ drop table if exists `passenger`;
 create table `passenger`
 (
     `id`          bigint      not null comment 'id',
-    `user_id`     bigint      not null comment '会员id',
+    `user_id`     bigint      null comment '会员id',
     `name`        varchar(20) not null comment '姓名|searchable',
     `id_card`     varchar(18) not null comment '身份证号',
     `mobile`      varchar(11) comment '手机号',
     `type`        char(1)     not null comment '乘客类型|枚举[PassengerType]',
-    `created_at` datetime(3) not null comment '创建时间',
-    `updated_at` datetime(3) not null comment '更新时间',
+    `created_at` datetime(3) null comment '创建时间',
+    `updated_at` datetime(3) null comment '更新时间',
     primary key (`id`),
     key `index_user_id` (`user_id`)
 ) default charset = utf8mb4 comment ='乘车人';
