31208f4a0b704a5e0eda3d63874919ddc22a60ce
KeepFocusL <1281438594@qq.com>
2025-04-20 10:19:09 +0800

    feat(user): 手动添加【乘车人】sql 语句和建表

--- sql/user.sql
+++ sql/user.sql
@@ -40,3 +40,19 @@ create table `course`
     `updated_at` datetime(3) null comment '课程更新时间' -- 例如，2025-01-01 12:34:56.789
 ) comment ='课程';
 
+-- 乘车人/乘客
+-- 乘车人不一定是网站的会员。比如你给爷爷奶奶等买票，他们只是单纯的乘车人身份
+drop table if exists `passenger`;
+create table `passenger`
+(
+    `id`          bigint      not null comment 'id',
+    `user_id`     bigint      not null comment '会员id',
+    `name`        varchar(20) not null comment '姓名|searchable',
+    `id_card`     varchar(18) not null comment '身份证号',
+    `mobile`      varchar(11) comment '手机号',
+    `type`        char(1)     not null comment '乘客类型|枚举[PassengerType]',
+    `created_at` datetime(3) not null comment '创建时间',
+    `updated_at` datetime(3) not null comment '更新时间',
+    primary key (`id`),
+    key `index_user_id` (`user_id`)
+) default charset = utf8mb4 comment ='乘车人';
