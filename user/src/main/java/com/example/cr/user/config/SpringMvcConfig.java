8a1de72d559613c9e2aa160695c0491091a864ae
KeepFocusL <1281438594@qq.com>
2025-04-20 11:39:49 +0800

    feat(user): 在 user 模块增加配置：启用 UserInterceptor

---
+++ user/src/main/java/com/example/cr/user/config/SpringMvcConfig.java
@@ -0,0 +1,25 @@
+package com.example.cr.user.config;
+
+import com.example.cr.common.interceptor.UserInterceptor;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+    @Resource
+    UserInterceptor userInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(userInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test",
+                        "/register",
+                        "/send-code",
+                        "/login"
+                );
+    }
+}
