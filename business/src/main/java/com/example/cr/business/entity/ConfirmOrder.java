87d29c8eea6a23668e1ab91c933c94eda44b8f44
KeepFocusL <1281438594@qq.com>
2025-04-24 09:12:12 +0800

    feat(business): 自动生成【确认订单】持久层代码

---
+++ business/src/main/java/com/example/cr/business/entity/ConfirmOrder.java
@@ -0,0 +1,136 @@
+package com.example.cr.business.entity;
+
+import java.util.Date;
+
+public class ConfirmOrder {
+    private Long id;
+
+    private Long userId;
+
+    private Date date;
+
+    private String trainCode;
+
+    private String start;
+
+    private String end;
+
+    private Long dailyTrainTicketId;
+
+    private String status;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    private String tickets;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public Long getDailyTrainTicketId() {
+        return dailyTrainTicketId;
+    }
+
+    public void setDailyTrainTicketId(Long dailyTrainTicketId) {
+        this.dailyTrainTicketId = dailyTrainTicketId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(String tickets) {
+        this.tickets = tickets;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", userId=").append(userId);
+        sb.append(", date=").append(date);
+        sb.append(", trainCode=").append(trainCode);
+        sb.append(", start=").append(start);
+        sb.append(", end=").append(end);
+        sb.append(", dailyTrainTicketId=").append(dailyTrainTicketId);
+        sb.append(", status=").append(status);
+        sb.append(", createdAt=").append(createdAt);
+        sb.append(", updatedAt=").append(updatedAt);
+        sb.append(", tickets=").append(tickets);
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ business/src/main/java/com/example/cr/business/mapper/ConfirmOrderMapper.java
@@ -0,0 +1,36 @@
+package com.example.cr.business.mapper;
+
+import com.example.cr.business.entity.ConfirmOrder;
+import com.example.cr.business.entity.ConfirmOrderExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ConfirmOrderMapper {
+    long countByExample(ConfirmOrderExample example);
+
+    int deleteByExample(ConfirmOrderExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(ConfirmOrder row);
+
+    int insertSelective(ConfirmOrder row);
+
+    List<ConfirmOrder> selectByExampleWithBLOBs(ConfirmOrderExample example);
+
+    List<ConfirmOrder> selectByExample(ConfirmOrderExample example);
+
+    ConfirmOrder selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("row") ConfirmOrder row, @Param("example") ConfirmOrderExample example);
+
+    int updateByExampleWithBLOBs(@Param("row") ConfirmOrder row, @Param("example") ConfirmOrderExample example);
+
+    int updateByExample(@Param("row") ConfirmOrder row, @Param("example") ConfirmOrderExample example);
+
+    int updateByPrimaryKeySelective(ConfirmOrder row);
+
+    int updateByPrimaryKeyWithBLOBs(ConfirmOrder row);
+
+    int updateByPrimaryKey(ConfirmOrder row);
+}
