b76797369caa6aa3f09f08b259c90c7127ab29d7
KeepFocusL <1281438594@qq.com>
2025-05-16 19:08:15 +0800

    feat(gateway): TestController@test 增加 environment.getProperty("server.port") 获取端口

--- user/src/main/java/com/example/cr/user/controller/TestController.java
+++ user/src/main/java/com/example/cr/user/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.example.cr.user.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +13,12 @@ public class TestController {
     @Value("${custom.name}")
     String customName;
 
+    @Autowired
+    Environment environment;
+
     @GetMapping("test")
     public String test(){
-        return "hello " + customName + " module!";
+        return "hello " + customName + " module! port = " + environment.getProperty("server.port");
     }
 
     @GetMapping("test1")
