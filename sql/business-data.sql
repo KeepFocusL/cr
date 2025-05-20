9b7966a8eba024956e25b3c1420b15995c3f297c
KeepFocusL <1281438594@qq.com>
2025-05-20 11:16:31 +0800

    refactor: 删除 ***-data.sql 插入语句中的 "xxx." 增加灵活性

--- sql/business-data.sql
+++ sql/business-data.sql
@@ -1,25 +1,25 @@
 -- 车站表
-INSERT INTO business.station (id, name, name_pinyin, name_py, created_at, updated_at) VALUES (1911228636137132032, '厦门', 'xiamen', 'XM', '2025-04-13 09:23:10.423', '2025-04-13 09:23:10.423');
-INSERT INTO business.station (id, name, name_pinyin, name_py, created_at, updated_at) VALUES (1911228680609337344, '厦门北', 'xiamenbei', 'XMB', '2025-04-13 09:23:21.026', '2025-04-13 09:23:21.026');
-INSERT INTO business.station (id, name, name_pinyin, name_py, created_at, updated_at) VALUES (1911228704349097984, '福州南', 'fuzhounan', 'FZN', '2025-04-13 09:23:26.686', '2025-04-13 09:23:26.686');
-INSERT INTO business.station (id, name, name_pinyin, name_py, created_at, updated_at) VALUES (1911228722762092544, '福州', 'fuzhou', 'FZ', '2025-04-13 09:23:31.076', '2025-04-13 09:23:31.076');
-INSERT INTO business.station (id, name, name_pinyin, name_py, created_at, updated_at) VALUES (1911228762733809664, '北京', 'beijing', 'BJ', '2025-04-13 09:23:40.606', '2025-04-13 09:23:40.606');
-INSERT INTO business.station (id, name, name_pinyin, name_py, created_at, updated_at) VALUES (1911228782132465664, '南京', 'nanjing', 'NJ', '2025-04-13 09:23:45.231', '2025-04-13 09:23:45.231');
+INSERT INTO station (id, name, name_pinyin, name_py, created_at, updated_at) VALUES (1911228636137132032, '厦门', 'xiamen', 'XM', '2025-04-13 09:23:10.423', '2025-04-13 09:23:10.423');
+INSERT INTO station (id, name, name_pinyin, name_py, created_at, updated_at) VALUES (1911228680609337344, '厦门北', 'xiamenbei', 'XMB', '2025-04-13 09:23:21.026', '2025-04-13 09:23:21.026');
+INSERT INTO station (id, name, name_pinyin, name_py, created_at, updated_at) VALUES (1911228704349097984, '福州南', 'fuzhounan', 'FZN', '2025-04-13 09:23:26.686', '2025-04-13 09:23:26.686');
+INSERT INTO station (id, name, name_pinyin, name_py, created_at, updated_at) VALUES (1911228722762092544, '福州', 'fuzhou', 'FZ', '2025-04-13 09:23:31.076', '2025-04-13 09:23:31.076');
+INSERT INTO station (id, name, name_pinyin, name_py, created_at, updated_at) VALUES (1911228762733809664, '北京', 'beijing', 'BJ', '2025-04-13 09:23:40.606', '2025-04-13 09:23:40.606');
+INSERT INTO station (id, name, name_pinyin, name_py, created_at, updated_at) VALUES (1911228782132465664, '南京', 'nanjing', 'NJ', '2025-04-13 09:23:45.231', '2025-04-13 09:23:45.231');
 
 
 -- 车次
-INSERT INTO business.train (id, code, type, start, start_pinyin, start_time, end, end_pinyin, end_time, created_at, updated_at) VALUES (1911229416948764672, 'G5114', 'G', '厦门', 'xiamen', '14:33:00', '福州', 'fuzhou', '16:09:00', '2025-04-13 09:26:16.583', '2025-04-13 09:26:16.583');
+INSERT INTO train (id, code, type, start, start_pinyin, start_time, end, end_pinyin, end_time, created_at, updated_at) VALUES (1911229416948764672, 'G5114', 'G', '厦门', 'xiamen', '14:33:00', '福州', 'fuzhou', '16:09:00', '2025-04-13 09:26:16.583', '2025-04-13 09:26:16.583');
 
 
 -- 火车车站
-INSERT INTO business.train_station (id, train_code, `index`, name, name_pinyin, in_time, out_time, stop_time, km, created_at, updated_at) VALUES (1911230072740777984, 'G5114', 0, '厦门', 'xiamen', '14:33:00', '14:33:00', '00:00:00', 0.00, '2025-04-13 09:28:52.936', '2025-04-13 09:28:52.936');
-INSERT INTO business.train_station (id, train_code, `index`, name, name_pinyin, in_time, out_time, stop_time, km, created_at, updated_at) VALUES (1911230297316397056, 'G5114', 1, '厦门北', 'xiamenbei', '14:55:00', '14:57:00', '00:02:00', 10.00, '2025-04-13 09:29:46.479', '2025-04-13 09:29:46.479');
-INSERT INTO business.train_station (id, train_code, `index`, name, name_pinyin, in_time, out_time, stop_time, km, created_at, updated_at) VALUES (1911230560727076864, 'G5114', 2, '福州南', 'fuzhounan', '15:52:00', '15:54:00', '00:02:00', 125.00, '2025-04-13 09:30:49.281', '2025-04-13 09:30:49.281');
-INSERT INTO business.train_station (id, train_code, `index`, name, name_pinyin, in_time, out_time, stop_time, km, created_at, updated_at) VALUES (1911230828139122688, 'G5114', 3, '福州', 'fuzhou', '16:09:00', '16:09:00', '00:00:00', 38.00, '2025-04-13 09:31:53.037', '2025-04-13 09:31:53.037');
+INSERT INTO train_station (id, train_code, `index`, name, name_pinyin, in_time, out_time, stop_time, km, created_at, updated_at) VALUES (1911230072740777984, 'G5114', 0, '厦门', 'xiamen', '14:33:00', '14:33:00', '00:00:00', 0.00, '2025-04-13 09:28:52.936', '2025-04-13 09:28:52.936');
+INSERT INTO train_station (id, train_code, `index`, name, name_pinyin, in_time, out_time, stop_time, km, created_at, updated_at) VALUES (1911230297316397056, 'G5114', 1, '厦门北', 'xiamenbei', '14:55:00', '14:57:00', '00:02:00', 10.00, '2025-04-13 09:29:46.479', '2025-04-13 09:29:46.479');
+INSERT INTO train_station (id, train_code, `index`, name, name_pinyin, in_time, out_time, stop_time, km, created_at, updated_at) VALUES (1911230560727076864, 'G5114', 2, '福州南', 'fuzhounan', '15:52:00', '15:54:00', '00:02:00', 125.00, '2025-04-13 09:30:49.281', '2025-04-13 09:30:49.281');
+INSERT INTO train_station (id, train_code, `index`, name, name_pinyin, in_time, out_time, stop_time, km, created_at, updated_at) VALUES (1911230828139122688, 'G5114', 3, '福州', 'fuzhou', '16:09:00', '16:09:00', '00:00:00', 38.00, '2025-04-13 09:31:53.037', '2025-04-13 09:31:53.037');
 
 -- 火车车厢（不完整）
-INSERT INTO business.train_carriage (id, train_code, `index`, seat_type, seat_count, row_count, col_count, created_at, updated_at) VALUES (1911231416759357440, 'G5114', 1, '1', 12, 3, 4, '2025-04-13 09:34:13.375', '2025-04-13 09:34:13.375');
-INSERT INTO business.train_carriage (id, train_code, `index`, seat_type, seat_count, row_count, col_count, created_at, updated_at) VALUES (1911231342205603840, 'G5114', 2, '2', 15, 3, 5, '2025-04-13 09:33:55.600', '2025-04-13 09:33:55.600');
-INSERT INTO business.train_carriage (id, train_code, `index`, seat_type, seat_count, row_count, col_count, created_at, updated_at) VALUES (1911231416759357140, 'G5114', 3, '2', 20, 4, 5, '2025-04-13 09:34:13.375', '2025-04-13 09:34:13.375');
+INSERT INTO train_carriage (id, train_code, `index`, seat_type, seat_count, row_count, col_count, created_at, updated_at) VALUES (1911231416759357440, 'G5114', 1, '1', 12, 3, 4, '2025-04-13 09:34:13.375', '2025-04-13 09:34:13.375');
+INSERT INTO train_carriage (id, train_code, `index`, seat_type, seat_count, row_count, col_count, created_at, updated_at) VALUES (1911231342205603840, 'G5114', 2, '2', 15, 3, 5, '2025-04-13 09:33:55.600', '2025-04-13 09:33:55.600');
+INSERT INTO train_carriage (id, train_code, `index`, seat_type, seat_count, row_count, col_count, created_at, updated_at) VALUES (1911231416759357140, 'G5114', 3, '2', 20, 4, 5, '2025-04-13 09:34:13.375', '2025-04-13 09:34:13.375');
 
 -- 火车座位(自动生成)
--- sql/user-data.sql
+++ sql/user-data.sql
@@ -1,9 +1,9 @@
 -- 用户
-INSERT INTO cr.user (id, name, email, password, mobile, created_at, updated_at) VALUES (1898625795887337472, null, null, '123456', '18123456789', null, null);
-INSERT INTO cr.user (id, name, email, password, mobile, created_at, updated_at) VALUES (1898625795887337472, null, null, null, '18012345678', null, null);
+INSERT INTO user (id, name, email, password, mobile, created_at, updated_at) VALUES (1898625795887337472, null, null, '123456', '18123456789', null, null);
+INSERT INTO user (id, name, email, password, mobile, created_at, updated_at) VALUES (1898625795887337472, null, null, null, '18012345678', null, null);
 
 -- 乘车人
-INSERT INTO cr.passenger (id, user_id, name, id_card, mobile, type, created_at, updated_at) VALUES (1913801127279857664, 1898625795887337472, '李四', '987654321987654321', '18987654321', '1', '2025-04-20 11:45:20.099', '2025-04-20 11:45:20.099');
-INSERT INTO cr.passenger (id, user_id, name, id_card, mobile, type, created_at, updated_at) VALUES (1913877759806017536, 1898625795887337472, '张山', '456456456456456456', '18456456456', '3', '2025-04-20 16:49:50.717', '2025-04-20 16:49:50.717');
-INSERT INTO cr.passenger (id, user_id, name, id_card, mobile, type, created_at, updated_at) VALUES (1915957156415410176, 1898625795887337472, '王五', '456456456456456456', '18456465456', '1', '2025-04-26 10:32:37.502', '2025-04-26 10:32:37.502');
-INSERT INTO cr.passenger (id, user_id, name, id_card, mobile, type, created_at, updated_at) VALUES (1920836723038883840, 1901617288252100608, '六六', '123456789123456789', '18012345678', '1', '2025-05-09 21:42:16.842', '2025-05-09 21:42:16.842');
+INSERT INTO passenger (id, user_id, name, id_card, mobile, type, created_at, updated_at) VALUES (1913801127279857664, 1898625795887337472, '李四', '987654321987654321', '18987654321', '1', '2025-04-20 11:45:20.099', '2025-04-20 11:45:20.099');
+INSERT INTO passenger (id, user_id, name, id_card, mobile, type, created_at, updated_at) VALUES (1913877759806017536, 1898625795887337472, '张山', '456456456456456456', '18456456456', '3', '2025-04-20 16:49:50.717', '2025-04-20 16:49:50.717');
+INSERT INTO passenger (id, user_id, name, id_card, mobile, type, created_at, updated_at) VALUES (1915957156415410176, 1898625795887337472, '王五', '456456456456456456', '18456465456', '1', '2025-04-26 10:32:37.502', '2025-04-26 10:32:37.502');
+INSERT INTO passenger (id, user_id, name, id_card, mobile, type, created_at, updated_at) VALUES (1920836723038883840, 1901617288252100608, '六六', '123456789123456789', '18012345678', '1', '2025-05-09 21:42:16.842', '2025-05-09 21:42:16.842');
