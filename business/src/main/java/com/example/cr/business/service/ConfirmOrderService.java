381675ac678f688006b81b3ee0c159ec2016ddb6
KeepFocusL <1281438594@qq.com>
2025-05-29 15:16:09 +0800

    feat(business): 引入 redisson，利用 redis 的分布式锁解决多实例超卖问题

--- business/src/main/java/com/example/cr/business/service/ConfirmOrderService.java
+++ business/src/main/java/com/example/cr/business/service/ConfirmOrderService.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.EnumUtil;
 import cn.hutool.core.util.NumberUtil;
 import cn.hutool.core.util.StrUtil;
@@ -22,11 +23,15 @@ import com.example.cr.business.response.ConfirmOrderResponse;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import jakarta.validation.Valid;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
 import com.example.cr.common.util.SnowflakeUtil;
@@ -49,6 +54,9 @@ public class ConfirmOrderService {
 
     @Autowired
     AfterConfirmOrderService afterConfirmOrderService;
+
+    @Autowired
+    private RedissonClient redissonClient;
 
     public PageResponse<ConfirmOrderResponse> list(ConfirmOrderListRequest request) {
         ConfirmOrderExample confirmOrderExample = new ConfirmOrderExample();
@@ -107,7 +115,33 @@ public class ConfirmOrderService {
         return confirmOrderMapper.deleteByExample(confirmOrderExample);
     }
 
-    public synchronized void confirm(@Valid ConfirmOrderRequest request) {
+    public void confirm(@Valid ConfirmOrderRequest request) {
+        String lockKey = DateUtil.formatDate(request.getDate()) + "-" + request.getTrainCode();
+        RLock lock = redissonClient.getLock("CONFIRM_ORDER_" + lockKey);
+        try {
+            // 尝试获取锁，最多等待 10 秒，如果超时则抛出异常
+            boolean tryLock = lock.tryLock(10, TimeUnit.SECONDS);
+            if (!tryLock) {
+                // 走到这里只是代表没抢到锁，并不知道车票被抢完了没，所以提示用户：服务器忙，请稍候重试
+                throw new CommonBusinessException("系统繁忙，请稍后重试");
+            }
+            // 抢到锁了！
+            // 处理订单逻辑...
+            // 如果有业务异常，直接抛出，不要catch
+            doConfirm(request);
+        } catch (InterruptedException e) {
+            // 获取锁被中断
+            // 跟上面类似，走到这里只是代表没抢到锁，并不知道车票被抢完了没，所以提示用户：服务器忙，请稍候重试
+            throw new CommonBusinessException("系统繁忙，请稍后重试");
+        } finally {
+            // 只有当前线程持有锁时才能释放锁
+            if (lock !=null && lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
+        }
+    }
+
+    private void doConfirm(@Valid ConfirmOrderRequest request) {
         // 业务数据校验【暂略】如：车次、余票是否存在；车次是否有效；tickets 条数是否大于 0；同乘客同车次的票是否已经买过了
 
         Date date = request.getDate();
