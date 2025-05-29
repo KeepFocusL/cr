dfd098993cdf03c425a7c14e718c597490e69fb7
KeepFocusL <1281438594@qq.com>
2025-05-29 11:50:03 +0800

    feat(business): 修改网关配置 uri: lb://business

--- business/src/main/java/com/example/cr/business/controller/TestController.java
+++ business/src/main/java/com/example/cr/business/controller/TestController.java
@@ -3,21 +3,25 @@ package com.example.cr.business.controller;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.EventListener;
 import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("test")
 public class TestController {
-
+    @Autowired
+    Environment environment;
     @GetMapping("")
     public String test(){
-        return "hello business module!";
+        return "hello business module! port = " + environment.getProperty("server.port");
     }
 
     private static final Logger log = LoggerFactory.getLogger(TestController.class);
