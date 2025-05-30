7a9fa834b88730bd10c24e06eeed90be8f4db3fc
KeepFocusL <1281438594@qq.com>
2025-05-30 16:02:30 +0800

    feat(cs): 增加 TestController 和对应的 test.http

---
+++ cs/src/main/java/com/example/cr/cs/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.cr.cs.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("test")
+    public String test(){
+        return "hello cs module!";
+    }
+}
